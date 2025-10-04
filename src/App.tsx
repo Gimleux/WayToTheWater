6e06eeb64f6410e0751264e87bf945d7d904e57d
Gimleux <56401687+Gimleux@users.noreply.github.com>
2025-10-04 10:22:56 +0200

    chore: Upgrade deps and switch from eslint to biome

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,39 @@
-import Countdown from 'react-countdown';
+import Countdown from "react-countdown";
 
-const RELEASE_DATE_STRING = '2024-02-29T13:10:00.000+01:00';
+const RELEASE_DATE_STRING = "2024-02-29T13:10:00.000+01:00";
 const RELEASE_DATE = new Date(RELEASE_DATE_STRING);
 
 function App() {
+	const renderer = ({
+		days,
+		hours,
+		minutes,
+		seconds,
+	}: {
+		days: number;
+		hours: number;
+		minutes: number;
+		seconds: number;
+		completed: boolean;
+	}) => (
+		<div>
+			<p>{days} days</p>
+			<p>{hours} hours</p>
+			<p>{minutes} minutes</p>
+			<p>{seconds} seconds</p>
+		</div>
+	);
 
-    const renderer = ({
-        days,
-        hours,
-        minutes,
-        seconds,
-    }: {
-        days: number;
-        hours: number;
-        minutes: number;
-        seconds: number;
-        completed: boolean;
-    }) => (
-        <div>
-            <p>{days} days</p>
-            <p>{hours} hours</p>
-            <p>{minutes} minutes</p>
-            <p>{seconds} seconds</p>
-        </div>
-    );
-
-    return (
-        <>
-            <h1 className="logo">Dune</h1>
-            <h2>Part Two</h2>
-            <div className="countdown">
-                <Countdown date={RELEASE_DATE} renderer={renderer} />
-            </div>
-            <div className="hint">until cinematic release in Germany</div>
-        </>
-    );
+	return (
+		<>
+			<h1 className="logo">Dune</h1>
+			<h2>Part Two</h2>
+			<div className="countdown">
+				<Countdown date={RELEASE_DATE} renderer={renderer} />
+			</div>
+			<div className="hint">until cinematic release in Germany</div>
+		</>
+	);
 }
 
 export default App;
